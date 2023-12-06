37f3a2c9cf8cbcb1647bd720bc0c163a18ff3302
Anderson Entwistle <46688047+aentwist@users.noreply.github.com>
2023-12-05 22:46:08 -0700

    style: sort destructured import members

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from "node:url";
+import { URL, fileURLToPath } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 import { defineConfig } from "vitest/config";
